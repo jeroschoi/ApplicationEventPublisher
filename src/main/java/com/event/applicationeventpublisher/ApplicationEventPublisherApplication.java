5989d48b602e396b7c520a51492408f9eab8ab0e
suwon82 <suwon82@gsretail.com>
2025-06-09 12:31:54 +0900

    Initial commit

---
+++ src/main/java/com/event/applicationeventpublisher/ApplicationEventPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.event.applicationeventpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationEventPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationEventPublisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/event/applicationeventpublisher/ApplicationEventPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.applicationeventpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationEventPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
